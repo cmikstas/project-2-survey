c256d51b5303c973555326adfe5d620c8d212892
NickM <nmikstas@alumni.scu.edu>
2019-12-07 10:25:25 -0700

    Disabled morgan

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ let app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
 
 //Set Handlebars.
 let exphbs = require("express-handlebars");
